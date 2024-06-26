a38eebbccbca09bd47855ddf6b6df8f1b2ad645f
sruthisureshgit123 <sruthi.suresh@ibm.com>
2024-06-26 15:35:51 +0530

    Add files via upload

---
+++ Python/Activities/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+# Create fixture
+@pytest.fixture
+def num_list():
+
+    # Create list
+    int_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    # Alternatively
+    # int_list = list(range(11))
+
+    return int_list
+
